9f3fcccfbedf7a462715148b690352cea78b30c9
Jonipeloni <103454689+Jonipeloni@users.noreply.github.com>
2023-11-04 14:55:23 +0100

    Erster richtiger commit

--- Code/crawler.py
+++ Code/crawler.py
@@ -1,6 +1,8 @@
+from queue import Queue
 import requests
 from bs4 import BeautifulSoup
 
+"""
 r = requests.get('https://www.joice-hamburg.de')
 print(r.status_code)
 print(r.headers)
@@ -13,3 +15,54 @@ for l in soup.find_all("a"):
     
     
 r = requests.get('https://vm009.rz.uos.de/')
+"""
+
+
+
+
+
+
+
+
+
+# Algorithm:
+# Initialize Cue with start URL
+q = Queue()
+url = "https://vm009.rz.uos.de/crawl/"
+
+q.put(url + "index.html")
+
+# inititalize variable
+current_url = 0
+history = []
+next_entry = 0
+end = 0
+print("hello")
+
+# While stack is not empty:
+while 0 == q.empty() or end < 10:
+    end = + 1
+    current_url = q.get()
+    history.append(current_url)
+    r = requests.get(current_url)
+    print(r.status_code)
+    print(r.headers)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    for l in soup.find_all("a"):
+        href = l['href']
+        next_entry = url + href
+        if next_entry not in history:
+            q.put(next_entry)
+
+
+print(history[:])
+
+
+
+
+
+# If not visited recently:
+# Get the content
+# Analyse it and updates the index
+# Analyse it, find links to other websites and add links to stack (push)
+# Update visited lst
--- Code/myapp.py
+++ Code/myapp.py
@@ -1,7 +1,12 @@
 from flask import Flask
+import whoosh
+
 #define global variable
 app = Flask(__name__)
 #wenn dieser path abgerufen wird, wird die folgende funktion gecalled
 @app.route("/")
 def start():
     return "Hello world"
+
+
+
